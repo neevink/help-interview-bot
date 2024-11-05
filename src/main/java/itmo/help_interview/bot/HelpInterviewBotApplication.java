e8a295f7caa8085379ae4decaee4851b803115d5
Evgeniy Ivanov <evonniy@mail.ru>
2024-11-05 18:59:07 +0300

    first commit init project

---
+++ src/main/java/itmo/help_interview/bot/HelpInterviewBotApplication.java
@@ -0,0 +1,13 @@
+package itmo.help_interview.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpInterviewBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpInterviewBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itmo/help_interview/bot/HelpInterviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.help_interview.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpInterviewBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
