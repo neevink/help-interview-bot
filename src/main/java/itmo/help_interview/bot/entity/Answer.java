69f776394dde237e8a920903e0bf595119a9b06e
Evgeniy Ivanov <evonniy@mail.ru>
2024-11-06 10:35:42 +0300

    little fix

--- src/main/java/itmo/help_interview/bot/entity/Answer.java
+++ src/main/java/itmo/help_interview/bot/entity/Answer.java
@@ -3,7 +3,6 @@ package itmo.help_interview.bot.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
